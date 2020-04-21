94c935c57f4e2edda20c290e3a2de08a071c6b52
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-04-20 21:58:52 -0500

    connect both services

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const weather = require('./weather/weather')
 /*place.getCityLatLng(argv.city)
     .then(response => console.log(response))*/
 
-weather.getWeather(-11.900000, -76.449997)
+/*weather.getWeather(-11.900000, -76.449997)
     .then(response => console.log(response))
-    .catch(err => console.error(err));
+    .catch(err => console.error(err));*/
+
+
+const getInfo = async (city) => {
+    try {
+        const coords = await place.getCityLatLng(city);
+        const temp = await weather.getWeather(coords.lat, coords.lng)
+
+        return `The weather of ${coords.city} is ${temp}.`;
+    } catch (e) {
+        return `${city} weather could not be determined.`;
+    }
+}
+
+getInfo(argv.city)
+    .then(console.log)
+    .catch(console.log);
